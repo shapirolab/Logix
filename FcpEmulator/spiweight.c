d13c2ac0ce3991745d11455b82a7a63605011bfa
bill <bill>
2006-06-27 14:47:54 +0000

    Added gaussian deal and delta delay.

--- FcpEmulator/spiweight.c
+++ FcpEmulator/spiweight.c
@@ -6,8 +6,9 @@
 /******************************  Examples *******************************/
 #define SQUARE 100
 #define POLY   101
-/************************************************************************/
-
+/******************************my defines *******************************/
+#define DELTA_DELAY 300
+#define GAUSSIAN_DELAY 301
 struct weighter {
   char *name;
   int index;
@@ -25,9 +26,10 @@ struct weighter weighttab[] = {
   /*****************************  Examples ******************************/
   { "square", SQUARE },
   { "poly", POLY },
-  /**********************************************************************/
-
-  { "default", DEFAULT }
+  /**********************************************************************/	
+  { "default", DEFAULT },
+   {"delta_delay",DELTA_DELAY},	
+   {"gaussian_delay" , GAUSSIAN_DELAY}
 };
 
 int spi_weight_name(int index, char **name) {
@@ -78,7 +80,49 @@ double spi_compute_bimolecular_weight(int method,
       break;
     }
   */
+      /*
+       * modeling a delay:
+       * using a gaussian with a given width(gaussbreite)
+       * plus an offset delay time
+       * thus needing three arguments:
+       * double currentTime
+       * double delayTime
+       * double sigma_square(variance)
+       * function: exp(-(currentTime - delayTime)^2/2* sigma_square)
+       */
 
+    case GAUSSIAN_DELAY: {
+      if(argn >= 0){
+	double currentTime = argv[0];
+	double delayTime   = argv[1];
+	double variance    = argv[2];
+	double delta = currentTime - delayTime;
+	result = rate*exp(-((delta*delta/2)*variance));
+      }
+      else
+	spi_compute_bimolecular_weight(DEFAULT,rate,sends,receives,argn,argv);
+      break;
+    }
+
+    /*
+     * modeling an offset delay:
+     * thus needing two arguments:
+     * double currentTime
+     * double delayTime
+     * function:
+     *   currentTime > delayTime ? result = rate*sends*receives:result= 0;
+     */
+
+    case DELTA_DELAY: {
+      if(argn >= 0){
+	double currentTime = argv[0];
+	double delayTime   = argv[1];
+	result = (currentTime > delayTime) ? rate*sends*receives : 0;
+	}
+      else
+	spi_compute_bimolecular_weight(DEFAULT,rate,sends,receives,argn,argv);
+      break;
+    }	
     case MICHAELIS: {
       double div = receives + argv[0];
       result = (argn >= 1 && div > 0) ?
@@ -139,6 +183,3 @@ double spi_compute_homodimerized_weight(int method, double rate, int dimers,
   }
   return result;
 }
-
-
-
